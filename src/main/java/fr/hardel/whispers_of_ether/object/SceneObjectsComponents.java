82e7fa55694b9ed709e4aa30f93da12c22c3ff9d
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2025-08-08 08:50:23 +0200

    Fixes

--- src/main/java/fr/hardel/whispers_of_ether/object/SceneObjectsComponents.java
+++ src/main/java/fr/hardel/whispers_of_ether/object/SceneObjectsComponents.java
@@ -4,8 +4,10 @@ import fr.hardel.whispers_of_ether.WhispersOfEther;
 import net.minecraft.util.Identifier;
 import org.ladysnake.cca.api.v3.component.ComponentKey;
 import org.ladysnake.cca.api.v3.component.ComponentRegistry;
+import org.ladysnake.cca.api.v3.component.ComponentFactory;
 import org.ladysnake.cca.api.v3.world.WorldComponentFactoryRegistry;
 import org.ladysnake.cca.api.v3.world.WorldComponentInitializer;
+import net.minecraft.world.World;
 
 public class SceneObjectsComponents implements WorldComponentInitializer {
     public static final ComponentKey<SceneObjectsComponent> SCENE_OBJECTS = ComponentRegistry
@@ -13,6 +15,7 @@ public class SceneObjectsComponents implements WorldComponentInitializer {
 
     @Override
     public void registerWorldComponentFactories(WorldComponentFactoryRegistry registry) {
-        registry.register(SCENE_OBJECTS, SceneObjectsComponent::new);
+        ComponentFactory<World, SceneObjectsComponent> factory = SceneObjectsComponent::new;
+        registry.register(SCENE_OBJECTS, factory);
     }
 }
